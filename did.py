b641ed63d61d79f48d43aa1a117466d582bc47dd
Michał Czuczman <michal.czuczman@gmail.com>
2011-12-11 20:33:23 +0100

    Print summary after each day, including the "current" job

--- did.py
+++ did.py
@@ -147,54 +147,82 @@ class JobList:
 class JobReport:
     def __init__(self, joblist):
         self.joblist = joblist
-        self.last_day = datetime.datetime.min
+        self.current_day = datetime.datetime.min
 
     def display(self):
         for job in self.joblist:
             self._print_job(job)
+        self._end_of_day()
 
-    def _start_day(self, day):
-        if day != self.last_day:
+    def _set_day(self, day):
+        if day != self.current_day:
+            self._end_of_day()
             print
             print day
-            self.last_day = day;
+            self.current_day = day
+            self.day_stats = {'T': datetime.timedelta(0),
+                              'B': datetime.timedelta(0),
+                              'C': datetime.timedelta(0)}
+
+    def _end_of_day(self):
+        if self.current_day == datetime.datetime.min:
+            return
+
+        task_delta = self.day_stats['T']
+        break_delta = self.day_stats['B']
+        current_delta = self.day_stats['C']
+        if current_delta > datetime.timedelta(0):
+            print "  Worked %s (+current=%s)   Slacked %s (+current=%s)" % (
+                    self.duration_to_string(task_delta),
+                    self.duration_to_string(task_delta + current_delta),
+                    self.duration_to_string(break_delta),
+                    self.duration_to_string(break_delta + current_delta))
+        else:
+            print "  Worked %-6s   Slacked %s" % (
+                    self.duration_to_string(task_delta),
+                    self.duration_to_string(break_delta))
+
 
     def _print_job(self, job):
         if isinstance(job, ArriveJob) or job.start == datetime.datetime.min:
-            self._start_day(job.end.date())
+            self._set_day(job.end.date())
             self._print_job_line(job)
         else:
-            self._start_day(job.start.date())
+            self._set_day(job.start.date())
             if job.start.date() == job.end.date():
                 self._print_job_line(job)
             else:
                 self._print_job_line(job)
                 day = job.start.date() + datetime.timedelta(1)
                 while day < job.end.date():
-                    self._start_day(day)
+                    self._set_day(day)
                     self._print_job_line(job)
                     day = day + datetime.timedelta(1)
-                self._start_day(job.end.date())
+                self._set_day(job.end.date())
                 self._print_job_line(job)
 
     def _print_job_line(self, job):
         start_time = False
         end_time = False
-        if job.starts_on(self.last_day):
+        if job.starts_on(self.current_day):
             start_time = job.start
-        if job.ends_on(self.last_day):
+        if job.ends_on(self.current_day):
             end_time = job.end
+        duration = job.duration(self.current_day)
+
+        if not isinstance(job, ArriveJob):
+            self.day_stats[job.letter()] += duration
 
         print "  %s .. %s  %s  %-30s  %s" % (
                 self.time_as_string(start_time),
                 self.time_as_string(end_time),
                 job.letter(),
                 job.name,
-                self.duration_to_string(job.duration(self.last_day)))
+                self.duration_to_string(duration))
 
     @staticmethod
     def duration_to_string(diff):
-        if diff is None or not diff:
+        if diff is None or diff is False:
             return ''
         hours = diff.days * 24 + diff.seconds / 3600
         minutes = (diff.seconds / 60) % 60
@@ -293,6 +321,8 @@ def main():
         name = " ".join(args)
         writer.append(now, name)
         joblist.push_job(now, name)
+    elif joblist.last_end().date() == datetime.date.today():
+        joblist.push_job(datetime.datetime.now(), "## current")
 
     report = JobReport(joblist)
     report.display()
