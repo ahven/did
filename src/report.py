f4c877d077b385e498bbc7081b799b96b5b75c4e
Michał Czuczman <michal.czuczman@gmail.com>
2012-01-01 18:42:54 +0100

    Extract collecting day stats from JobReport

--- src/report.py
+++ src/report.py
@@ -19,7 +19,46 @@ Fifth Floor, Boston, MA  02110-1301  USA
 """
 
 import datetime
-from job import ArriveJob
+from job import ArriveJob, BreakJob, CurrentJob, TaskJob
+
+
+class JobDayStats:
+    def __init__(self, joblist, day):
+        self.joblist = joblist
+        self.day = day
+        self.work_time = datetime.timedelta(0)
+        self.break_time = datetime.timedelta(0)
+        self.current_time = datetime.timedelta(0)
+        self._analyze()
+
+    def _analyze(self):
+        i = self.joblist.first_job_for_day(self.day)
+        if i is not None:
+            while i < len(self.joblist) and \
+                    self.joblist[i].start.date() <= self.day:
+                self._analyze_job(self.joblist[i])
+                i += 1
+
+    def _analyze_job(self, job):
+        start = job.start
+        end = job.end
+        midnight = datetime.time(0, 0, 0)
+
+        if start.date() < self.day:
+            start = datetime.datetime.combine(self.day, midnight)
+        if end.date() > self.day:
+            end = datetime.datetime.combine(self.day, midnight)
+            end += datetime.timedelta(1)
+
+        diff = end - start
+
+        if isinstance(job, TaskJob):
+            self.work_time += diff;
+        elif isinstance(job, BreakJob):
+            self.break_time += diff
+        elif isinstance(job, CurrentJob):
+            self.current_time += diff
+
 
 class JobReport:
     def __init__(self, joblist):
@@ -27,68 +66,48 @@ class JobReport:
         self.current_day = datetime.datetime.min
 
     def display(self):
-        for job in self.joblist:
-            self._print_job(job)
-        self._end_of_day()
+        for day in self.joblist.days():
+            self._print_day(day)
 
-    def _set_day(self, day):
-        if day != self.current_day:
-            self._end_of_day()
-            print
-            print day
-            self.current_day = day
-            self.day_stats = {'T': datetime.timedelta(0),
-                              'B': datetime.timedelta(0),
-                              'C': datetime.timedelta(0)}
+    def _print_day(self, day):
+        self._print_day_header(day)
 
-    def _end_of_day(self):
-        if self.current_day == datetime.datetime.min:
-            return
+        i = self.joblist.first_job_for_day(day)
+        if i is not None:
+            while i < len(self.joblist) and self.joblist[i].start.date() <= day:
+                self._print_job_line(self.joblist[i], day)
+                i += 1
 
-        task_delta = self.day_stats['T']
-        break_delta = self.day_stats['B']
-        current_delta = self.day_stats['C']
-        if current_delta > datetime.timedelta(0):
+        self._print_day_footer(day)
+
+    def _print_day_header(self, day):
+        print
+        print day
+
+    def _print_day_footer(self, day):
+        stats = JobDayStats(self.joblist, day)
+        if stats.current_time > datetime.timedelta(0):
             print "  Worked %s (+current=%s)   Slacked %s (+current=%s)" % (
-                    self.duration_to_string(task_delta),
-                    self.duration_to_string(task_delta + current_delta),
-                    self.duration_to_string(break_delta),
-                    self.duration_to_string(break_delta + current_delta))
+                self.duration_to_string(stats.work_time),
+                self.duration_to_string(stats.work_time + stats.current_time),
+                self.duration_to_string(stats.break_time),
+                self.duration_to_string(stats.break_time + stats.current_time))
         else:
             print "  Worked %-6s   Slacked %s" % (
-                    self.duration_to_string(task_delta),
-                    self.duration_to_string(break_delta))
+                    self.duration_to_string(stats.work_time),
+                    self.duration_to_string(stats.break_time))
 
-
-    def _print_job(self, job):
-        if isinstance(job, ArriveJob) or job.start == datetime.datetime.min:
-            self._set_day(job.end.date())
-            self._print_job_line(job)
-        else:
-            self._set_day(job.start.date())
-            if job.start.date() == job.end.date():
-                self._print_job_line(job)
-            else:
-                self._print_job_line(job)
-                day = job.start.date() + datetime.timedelta(1)
-                while day < job.end.date():
-                    self._set_day(day)
-                    self._print_job_line(job)
-                    day = day + datetime.timedelta(1)
-                self._set_day(job.end.date())
-                self._print_job_line(job)
-
-    def _print_job_line(self, job):
+    def _print_job_line(self, job, day):
         start_time = False
         end_time = False
-        if job.starts_on(self.current_day):
+        if job.starts_on(day):
             start_time = job.start
-        if job.ends_on(self.current_day):
+        if job.ends_on(day):
             end_time = job.end
-        duration = job.duration(self.current_day)
+        duration = job.duration(day)
 
-        if not isinstance(job, ArriveJob):
-            self.day_stats[job.letter()] += duration
+        if isinstance(job, ArriveJob) and end_time == False:
+            return
 
         print "  %s .. %s  %s  %-30s  %s" % (
                 self.time_as_string(start_time),
