1643632ab4a0452df9051761a2e0079c8d9b66e9
Michał Czuczman <michal.czuczman@gmail.com>
2012-01-07 20:33:31 +0100

    Move JobDayStats to a new file "stats.py"

--- src/report.py
+++ src/report.py
@@ -21,44 +21,7 @@ Fifth Floor, Boston, MA  02110-1301  USA
 import datetime
 from colorama import Fore, Style
 from job import ArriveJob, BreakJob, CurrentJob, TaskJob
-
-
-class JobDayStats:
-    def __init__(self, joblist, day):
-        self.joblist = joblist
-        self.day = day
-        self.work_time = datetime.timedelta(0)
-        self.break_time = datetime.timedelta(0)
-        self.current_time = datetime.timedelta(0)
-        self._analyze()
-
-    def _analyze(self):
-        i = self.joblist.first_job_for_day(self.day)
-        if i is not None:
-            while i < len(self.joblist) and \
-                    self.joblist[i].start.date() <= self.day:
-                self._analyze_job(self.joblist[i])
-                i += 1
-
-    def _analyze_job(self, job):
-        start = job.start
-        end = job.end
-        midnight = datetime.time(0, 0, 0)
-
-        if start.date() < self.day:
-            start = datetime.datetime.combine(self.day, midnight)
-        if end.date() > self.day:
-            end = datetime.datetime.combine(self.day, midnight)
-            end += datetime.timedelta(1)
-
-        diff = end - start
-
-        if isinstance(job, TaskJob):
-            self.work_time += diff;
-        elif isinstance(job, BreakJob):
-            self.break_time += diff
-        elif isinstance(job, CurrentJob):
-            self.current_time += diff
+from stats import JobDayStats
 
 
 def color_for_job(job):
