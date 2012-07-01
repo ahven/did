d958a72c92d9ab58843530e139c2d3a638829422
Michał Czuczman <michal.czuczman@gmail.com>
2012-07-01 16:02:15 +0200

    Add DayRange with support for an exact day and a first..last range

---
+++ src/DayRange.py
@@ -0,0 +1,106 @@
+'''
+Created on 2012-06-04
+
+@author: ahven
+'''
+
+import datetime
+import re
+
+class InvalidRangeFormatError(Exception):
+
+    def __init__(self, range_specs):
+        self.range_specs = range_specs
+
+    def __str__(self):
+        return "Invalid range: " + self.range_specs
+
+
+class PatternDecoratorDispatcher:
+    """
+    Decorator-based regex pattern dispatcher
+    """
+
+    def __init__(self):
+        self.patterns = []
+
+    def register(self, pattern):
+        """Decorator to register a dispatching function"""
+        regex = re.compile(pattern)
+        def wrap(func):
+            self.patterns.append( (regex, func) )
+            return func
+        return wrap
+
+    def dispatch(self, obj, text):
+        """
+        Call the first registered function whose pattern matches given text.
+        Passes a tuple containing all the subgroups of the match.
+        Return True if there was a match and a function was called.
+        Return False otherwise.
+        """
+        for regex, func in self.patterns:
+            match = regex.search(text)
+            if match:
+                func(obj, match.groups())
+                return True
+        return False
+
+
+class DayRange:
+    '''
+    A range of days
+
+    One day formats:
+     * YYYY-MM-DD
+
+    Range formats:
+     * <first>..<last>
+    '''
+
+    patterns = PatternDecoratorDispatcher()
+
+    def __init__(self, range_specs):
+        '''
+        Constructor
+        '''
+        self.set_range_text(range_specs)
+
+    def first_day(self):
+        return self._first
+
+    def last_day(self):
+        return self._last
+
+    def is_valid(self):
+        return self._first <= self._last
+
+    def set_range_text(self, range_specs):
+        if not DayRange.patterns.dispatch(self, range_specs):
+            raise InvalidRangeFormatError(range_specs)
+
+    def set_range(self, first, last):
+        self._first = first
+        self._last = last
+
+    def contains(self, date):
+        return self._first <= date and date <= self._last
+
+    @patterns.register(
+            r'^([12][0-9]{3})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$')
+    def _pattern_yyyy_mm_dd(self, groups):
+        self._first = datetime.date(
+                int(groups[0]), int(groups[1]), int(groups[2]))
+        self._last = self._first
+
+    @patterns.register(r'^([^.]*)\.\.([^.]*)$')
+    def _pattern_first_last(self, groups):
+        if groups[0] == '':
+            self._first = datetime.date.min
+        else:
+            self._first = DayRange(groups[0])._first
+
+        if groups[1] == '':
+            self._last = datetime.date.max
+        else:
+            self._last = DayRange(groups[1])._last
---
+++ src/DayRangeTest.py
@@ -0,0 +1,36 @@
+'''
+Created on 2012-06-29
+
+@author: ahven
+'''
+import unittest
+import datetime
+from DayRange import DayRange, InvalidRangeFormatError
+
+def d(year, month, day):
+    return datetime.date(year, month, day)
+
+
+class DayRangeTest(unittest.TestCase):
+
+    def verify(self, text, expected_first, expected_last):
+        day_range = DayRange(text)
+        self.assertEqual(day_range.first_day(), expected_first)
+        self.assertEqual(day_range.last_day(), expected_last)
+
+    def verifyInvalid(self, text):
+        self.assertRaises(InvalidRangeFormatError, DayRange, text)
+
+    def testIso(self):
+        self.verify("2012-05-30", d(2012, 5, 30), d(2012, 5, 30))
+        self.verifyInvalid("2012-13-01")
+        self.verifyInvalid("2012-00-01")
+        self.verifyInvalid("2012-12-32")
+
+    def testRange(self):
+        self.verify("2012-03-30..2012-06-20", d(2012, 3, 30), d(2012, 6, 20))
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
