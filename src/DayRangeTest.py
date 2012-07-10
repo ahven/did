e0566ef01ece7fdfce6f25c0909aa5cdc261541c
Michał Czuczman <michal.czuczman@gmail.com>
2012-07-10 22:21:07 +0200

    DayRangeTest: Simplify specifying tests for one-day ranges
    Almost all test cases had the same first and last day specified.
    So let's give a separate method for this common case.

--- src/DayRangeTest.py
+++ src/DayRangeTest.py
@@ -18,11 +18,14 @@ class DayRangeTest(unittest.TestCase):
         self.assertEqual(day_range.first_day(), expected_first)
         self.assertEqual(day_range.last_day(), expected_last)
 
+    def verifyOne(self, text, expected):
+        self.verify(text, expected, expected)
+
     def verifyInvalid(self, text):
         self.assertRaises(InvalidRangeFormatError, DayRange, text)
 
     def testIso(self):
-        self.verify("2012-05-30", d(2012, 5, 30), d(2012, 5, 30))
+        self.verifyOne("2012-05-30", d(2012, 5, 30))
         self.verifyInvalid("2012-13-01")
         self.verifyInvalid("2012-00-01")
         self.verifyInvalid("2012-12-32")
@@ -30,7 +33,7 @@ class DayRangeTest(unittest.TestCase):
         self.verifyInvalid("2012-11-31")
 
     def testYYYYMMDD(self):
-        self.verify("20120530", d(2012, 5, 30), d(2012, 5, 30))
+        self.verifyOne("20120530", d(2012, 5, 30))
         self.verifyInvalid("20121301")
         self.verifyInvalid("20120001")
         self.verifyInvalid("20121232")
@@ -42,13 +45,11 @@ class DayRangeTest(unittest.TestCase):
         yesterday = today - datetime.timedelta(1)
         tomorrow = today + datetime.timedelta(1)
 
-        self.verify("1-01", d(today.year, 1, 1), d(today.year, 1, 1))
-        self.verify("01-01", d(today.year, 1, 1), d(today.year, 1, 1))
-        self.verify("%d-%02d" % (today.month, today.day), today, today)
-        self.verify("%d-%02d" % (yesterday.month, yesterday.day),
-                    yesterday, yesterday)
-        self.verify("%d-%02d" % (tomorrow.month, tomorrow.day),
-                    d(tomorrow.year - 1, tomorrow.month, tomorrow.day),
+        self.verifyOne("1-01", d(today.year, 1, 1))
+        self.verifyOne("01-01", d(today.year, 1, 1))
+        self.verifyOne("%d-%02d" % (today.month, today.day), today)
+        self.verifyOne("%d-%02d" % (yesterday.month, yesterday.day), yesterday)
+        self.verifyOne("%d-%02d" % (tomorrow.month, tomorrow.day),
                     d(tomorrow.year - 1, tomorrow.month, tomorrow.day))
 
         self.verifyInvalid("13-01")
@@ -59,27 +60,23 @@ class DayRangeTest(unittest.TestCase):
         today = datetime.date.today()
         yesterday = today - datetime.timedelta(1)
 
-        self.verify("1", d(today.year, today.month, 1),
-                    d(today.year, today.month, 1))
-        self.verify("01", d(today.year, today.month, 1),
-                    d(today.year, today.month, 1))
-        self.verify("%d" % yesterday.day, yesterday, yesterday)
-        self.verify("%d" % today.day, today, today)
+        self.verifyOne("1", d(today.year, today.month, 1))
+        self.verifyOne("01", d(today.year, today.month, 1))
+        self.verifyOne("%d" % yesterday.day, yesterday)
+        self.verifyOne("%d" % today.day, today)
 
         self.verifyInvalid("32")
 
     def testXDaysAgo(self):
         today = datetime.date.today()
-        self.verify("-1", today - datetime.timedelta(1),
-                    today - datetime.timedelta(1))
-        self.verify("-300", today - datetime.timedelta(300),
-                    today - datetime.timedelta(300))
-        self.verify("-0", today, today)
+        self.verifyOne("-1", today - datetime.timedelta(1))
+        self.verifyOne("-300", today - datetime.timedelta(300))
+        self.verifyOne("-0", today)
         self.verifyInvalid("-01")
 
     def testToday(self):
         today = datetime.date.today()
-        self.verify("0", today, today)
+        self.verifyOne("0", today)
 
     def testRange(self):
         self.verify("2012-03-30..2012-06-20", d(2012, 3, 30), d(2012, 6, 20))
