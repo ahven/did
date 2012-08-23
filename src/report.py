ahven/did

commit 489f6edeb82a9584d59a56db0818616689f3be0d

report.py: Extract SessionChronologicalDisplay()
There will be multiple ways to print intervals for a session, so each
should be implemented in a separate class.

src/report.py
@@ -45,50 +45,10 @@ def time_as_string(time):
     else:
         return "     "
 
-
-class JobReport:
-    def __init__(self, worklog, stats_factory):
-        self.worklog = worklog
-        self.stats_factory_ = stats_factory
-
-    def display(self, day_range):
-        index = 0
-        sessions = self.worklog.sessions()
-        self.total_overtime = datetime.timedelta(0)
-        for session in sessions:
-            index += 1
-            stats = self.stats_factory_.new_session_stats(session)
-            self.total_overtime += stats.overhours()
-            if day_range.contains(session.start().date()):
-                self._print_session(session, stats)
-
-    def _print_session(self, session, stats):
-        self._print_day_header(session.start().date())
-
-        if len(session.intervals()) == 0:
-            self._print_log_line(
-                    "", time_as_string(session.start()),
-                    Foreground.red, "arrive", "", "")
-
+class SessionChronologicalDisplay:
+    def display(self, session):
         for interval in session.intervals():
             self._print_interval(interval)
-
-        self._print_day_footer(session, stats)
-
-    def _print_day_header(self, day):
-        print
-        print Foreground.green + str(day) + Attributes.reset
-
-    def _print_day_footer(self, session, stats):
-        work_time = stats.time_worked()
-        break_time = stats.time_slacked()
-        overtime = stats.overhours();
-
-        print "  Worked %-6s   Slacked %-6s   Overtime %-6s (total %s)" % (
-                    duration_to_string(work_time),
-                    duration_to_string(break_time),
-                    duration_to_string(overtime),
-                    duration_to_string(self.total_overtime))
 
     def _print_interval(self, interval):
         if interval.is_break():
@@ -124,3 +84,49 @@ class JobReport:
                 duration_color,
                 duration,
                 Attributes.reset)
+
+
+class JobReport:
+    def __init__(self, worklog, stats_factory,
+                session_display=SessionChronologicalDisplay()):
+        self.worklog = worklog
+        self.stats_factory_ = stats_factory
+        self.session_display = session_display
+
+    def display(self, day_range):
+        index = 0
+        sessions = self.worklog.sessions()
+        self.total_overtime = datetime.timedelta(0)
+        for session in sessions:
+            index += 1
+            stats = self.stats_factory_.new_session_stats(session)
+            self.total_overtime += stats.overhours()
+            if day_range.contains(session.start().date()):
+                self._print_session(session, stats)
+
+    def _print_session(self, session, stats):
+        self._print_day_header(session.start().date())
+
+        if len(session.intervals()) == 0:
+            self._print_log_line(
+                    "", time_as_string(session.start()),
+                    Foreground.red, "arrive", "", "")
+
+        self.session_display.display(session)
+
+        self._print_day_footer(session, stats)
+
+    def _print_day_header(self, day):
+        print
+        print Foreground.green + str(day) + Attributes.reset
+
+    def _print_day_footer(self, session, stats):
+        work_time = stats.time_worked()
+        break_time = stats.time_slacked()
+        overtime = stats.overhours();
+
+        print "  Worked %-6s   Slacked %-6s   Overtime %-6s (total %s)" % (
+                    duration_to_string(work_time),
+                    duration_to_string(break_time),
+                    duration_to_string(overtime),
+                    duration_to_string(self.total_overtime))

commit fe2264dd19d9c59282cf6dd9b9643adcccba166a

report.py: Fix error raised when running "did arrive"
This error was introduced in 489f6edeb82a9584d59a56db0818616689f3be0d
("report.py: Extract SessionChronologicalDisplay()").

The following output was printed on running `did arrive`:

  2012-08-23
  Traceback (most recent call last):
    File "src/did.py", line 149, in <module>
      did.run()
    File "src/did.py", line 67, in run
      report.display(DayRange(self.options.range))
    File "/home/ahven/dev/time-management/did/src/report.py", line 140, in display
      self._print_session(session, stats)
    File "/home/ahven/dev/time-management/did/src/report.py", line 146, in _print_session
      self._print_log_line(
  AttributeError: JobReport instance has no attribute '_print_log_line'

src/report.py
@@ -73,7 +73,7 @@ class SessionChronologicalDisplay:
         if interval.is_assumed():
             name += " (assumed)"
 
-        self._print_log_line(
+        self.print_log_line(
                 time_as_string(interval.start()),
                 time_as_string(interval.end()),
                 get_name_color(interval.is_break(), interval.is_assumed()),
@@ -81,7 +81,7 @@ class SessionChronologicalDisplay:
                 get_duration_color(interval.is_break(), interval.is_assumed()),
                 duration_to_string(interval.end() - interval.start()))
 
-    def _print_log_line(
+    def print_log_line(
             self, start, end, name_color, name, duration_color, duration):
         print "  %5s .. %5s  %s%-30s%s  %s%s%s" % (
                 start,
@@ -143,7 +143,7 @@ class JobReport:
         self._print_day_header(session.start().date())
 
         if len(session.intervals()) == 0:
-            self._print_log_line(
+            self.session_display.print_log_line(
                     "", time_as_string(session.start()),
                     Foreground.red, "arrive", "", "")
 